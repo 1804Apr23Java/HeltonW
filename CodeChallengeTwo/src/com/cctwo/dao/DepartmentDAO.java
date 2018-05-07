a74d9c03b8a2acc938862d695283e8db04b50931
William Helton <william_helton2@mymail.eku.edu>
2018-05-07 10:57:18 -0400

    department dao interface done

---
+++ CodeChallengeTwo/src/com/cctwo/dao/DepartmentDAO.java
@@ -0,0 +1,25 @@
+package com.cctwo.dao;
+
+public class DepartmentDAO implements DepartmentDAOInterface {
+	
+	private String filename = "connection.properties";
+
+	@Override
+	public boolean addDepartment(String deptName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateDepartment(int deptID, String deptName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteDepartment(int departmentID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
