33bfac9aa0aba60bb83e42e868cb94502f32ca03
zmacaulay <zmacaulay@gmail.com>
2018-05-15 14:49:59 -0400

    website updated

--- AwesomeWebSite/movieVotes.js
+++ AwesomeWebSite/movieVotes.js
@@ -1,11 +1,16 @@
 function vote() {
-	var selection = document.getElementById("chooseMovie");
+	var selection = document.getElementById("movieSelector");
 	var button = document.getElementById("voteButton");
 	button.addEventListener("click", results);
-	function results() {
-		var resultsDiv = document.getElementById("chosenMovie");
-		resultsDiv.innerHTML += "Congratulations!  You've chosen " + selection.options[selection.selectedIndex].text;
-	};
 };
+	function results() {
+		var resultsDiv = document.getElementById("chosenMovieDiv");
+		var paragraph = document.createElement("P");
+		paragraph.innerHTML = "Test string";
+		resultsDiv.appendChild(paragraph);
+		//resultsDiv.innerHTML += "Congratulations!  You've chosen "; 
+		//selection.options[selection.selectedIndex].text;
+	};
+//};
 vote();
 
