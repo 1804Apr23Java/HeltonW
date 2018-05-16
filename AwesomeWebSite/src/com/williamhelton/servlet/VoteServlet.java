3bc83d7d02c5cc563324ff9117f27477adda9431
William Helton <william_helton2@mymail.eku.edu>
2018-05-16 15:32:08 -0400

    database servlet call works, prints in console

--- AwesomeWebSite/src/com/williamhelton/servlet/VoteServlet.java
+++ AwesomeWebSite/src/com/williamhelton/servlet/VoteServlet.java
@@ -2,6 +2,13 @@ package com.williamhelton.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +16,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.williamhelton.connectionutil.ConnectionUtil;
+
 public class VoteServlet extends HttpServlet {
+	
+	
 
 	/**
 	 * 
@@ -27,7 +38,13 @@ public class VoteServlet extends HttpServlet {
 		//obtain request parameters
 		String result = req.getParameter("vote");
 		System.out.println(result);
-		
+		try {
+			System.out.println(getMovie(result));
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		//calculate answer
 		
 		
@@ -49,4 +66,29 @@ public class VoteServlet extends HttpServlet {
 		
 	}
 	
+	public int getMovie(String movie) throws ClassNotFoundException {
+		String filename = "connection.properties";
+		PreparedStatement p = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "master");
+			String sql = "SELECT ANYTHING FROM MOVIES WHERE MOVIE_TITLE = ?";
+			p = con.prepareStatement(sql);
+			p.setString(1, movie);
+			ResultSet rs = p.executeQuery();
+			System.out.println("query executed");
+			int result=-1;
+			while(rs.next()) {
+				result = rs.getInt("ANYTHING");
+				System.out.println(result + " is result");
+			}
+			
+			
+			return -1;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
